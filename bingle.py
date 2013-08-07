16fb4c1fd03aab615a09d22408c57fab16841da8
Diederik van Liere <dvanliere@wikimedia.org>
2013-08-07 12:01:38 +0200

    Add support for configurable mingle property fields

--- bingle.py
+++ bingle.py
@@ -16,7 +16,10 @@ if __name__ == "__main__":
 	auth = {'username': config.get('auth','username'), 'password': config.get('auth','password')}
 	debug = config.getboolean('debug','debug')
 	picklePath = config.get('paths', 'picklePath')
-	apiBaseUrl = config.get('urls','mingleApiBase')
+	apiBaseUrl = config.get('urls', 'mingleApiBase')
+	bugCard = config.get('mingle', 'bugCard')
+	propertiesRaw = config.get('mingle', 'properties')
+	properties = dict((key,value) for key,value in (prop.split(',') for prop in propertiesRaw.split(';') if prop.find(',') > -1))	
 
 	bingle = Bingle( debug=debug, picklePath=picklePath, feedUrl=config.get('urls','bugzillaFeed') )
 
@@ -25,23 +28,26 @@ if __name__ == "__main__":
 
 	for entry in bingle.getFeedEntries():
 		# look for card
-		foundBugs = mingle.findCardByName( 'Bug', entry.title )
+		print entry
+		foundBugs = mingle.findCardByName( bugCard, entry.title )
 		bingle.info( mingle.dumpRequest() ) 
 		if len( foundBugs ) > 0:
 			continue
 		cardParams = {
 			'card[name]': entry.title.encode('ascii','ignore'),
-			'card[card_type_name]':'bug',
+			'card[card_type_name]': bugCard,
 			'card[description]': entry.id.encode('ascii','ignore'), # URL to bug
-			'card[properties][][name]': 'Iteration',
-			'card[properties][][value]': '(Current iteration)',
 			'card[created_by]': auth['username']
 		}
 		cardLocation = mingle.addCard( cardParams )
 		bingle.info( mingle.dumpRequest() ) 
-		cardParams = {
-			'card[properties][][name]': 'status',
-			'card[properties][][value]': 'In analysis'
-		}
-		mingle.updateCard( cardLocation, cardParams )
+
+		#properties	
+		for prop in properties:
+			cardParams = {
+				'card[properties][][name]': key,
+				'card[properties][][value]': value
+			}
+			mingle.updateCard( cardLocation, cardParams )
+		
 		bingle.info( mingle.dumpRequest() )
